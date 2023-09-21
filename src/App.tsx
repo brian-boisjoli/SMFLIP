47f5b5cd66dbde7c2228864f1b3effcc49dfabd5
brian-boisjoli <90005524+brian-boisjoli@users.noreply.github.com>
2023-09-21 10:14:53 -0400

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,34 @@
-import React from 'react'
-import { Route, Routes, useLocation } from 'react-router-dom'
-import { Card } from './components/Card'
-import { Header } from './components/Header'
-import { GAMES } from './games'
-import { ErrorHandlers } from './ui/ErrorHandlers'
-import { Game } from './ui/Game'
-import { Home } from './ui/Home'
-import { RecentPlays } from './ui/RecentPlays'
-import { UserButton } from './ui/UserButton'
-
+import React from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
+import { Card } from "./components/Card";
+import { Header } from "./components/Header";
+import { GAMES } from "./games";
+import { ErrorHandlers } from "./ui/ErrorHandlers";
+import { Game } from "./ui/Game";
+import { Home } from "./ui/Home";
+import { RecentPlays } from "./ui/RecentPlays";
+import { UserButton } from "./ui/UserButton";
 
 export function App() {
-  const location = useLocation()
+  const location = useLocation();
 
-  React.useEffect(() =>
-    document.body.scrollTo({
-      top: 0,
-      left: 0,
-    })
-  , [location.key])
+  React.useEffect(
+    () =>
+      document.body.scrollTo({
+        top: 0,
+        left: 0,
+      }),
+    [location.key]
+  );
 
   return (
     <>
       <ErrorHandlers />
 
       <Header>
+        <div className="back_button">
+          <a href="https://solarmoon.xyz/">Back to Website</a>
+        </div>
         <UserButton />
       </Header>
 
@@ -32,9 +36,7 @@ export function App() {
         <Route path="/" element={<Home />} />
         <Route path="/:shortName" element={<Game />} />
       </Routes>
-
-
       <RecentPlays />
     </>
-  )
+  );
 }
