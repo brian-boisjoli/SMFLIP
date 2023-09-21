264b8eb35fae2b1f90a81c19063f5638bec1c35e
brian-boisjoli <90005524+brian-boisjoli@users.noreply.github.com>
2023-09-21 14:11:21 -0400

    Add files via upload

--- src/games/Flip/index.ts
+++ src/games/Flip/index.ts
@@ -5,9 +5,10 @@ export default {
   name: 'Flip',
   short_name: 'flip',
   description: `
-    Pick Heads or Tails. Double your money or go broke. Simple as.
+    Pick Heads or Tails. Double your money or go broke. Simple as that.
   `,
-  creator: 'DwRFGbjKbsEhUMe5at3qWvH7i8dAJyhhwdnFoZMnLVRV',
+  /*creator: 'DwRFGbjKbsEhUMe5at3qWvH7i8dAJyhhwdnFoZMnLVRV',*/
+  creator: '5AZYZb7sfB5K7P2GCWnB6v2G7urQ3LjoKtKRop8A79DA',
   image,
   theme_color: 'rgb(255 218 121)',
   app: React.lazy(() => import('./App')),
