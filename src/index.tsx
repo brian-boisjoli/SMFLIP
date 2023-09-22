bd482736e5ef1d263aefac4c9644f59a9fd05ccb
brian-boisjoli <90005524+brian-boisjoli@users.noreply.github.com>
2023-09-22 16:13:01 -0400

    Add files via upload

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,34 @@
-import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
-import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
-import '@solana/wallet-adapter-react-ui/styles.css'
-import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets'
-import { Gamba } from 'gamba/react'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { App } from './App'
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import "@solana/wallet-adapter-react-ui/styles.css";
+import {
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+import { Gamba } from "gamba/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { MemoryRouter } from "react-router-dom";
+import { App } from "./App";
 
-import './styles.css'
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root')!)
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 
 function Root() {
-  const wallets = React.useMemo(
-    () => [
-      new SolflareWalletAdapter(),
-    ],
-    [],
-  )
+  const wallets = React.useMemo(() => [new SolflareWalletAdapter()], []);
 
   return (
-    <BrowserRouter>
+    <MemoryRouter>
       <ConnectionProvider
         endpoint={import.meta.env.GAMBA_SOLANA_RPC}
-        config={{ wsEndpoint: import.meta.env.GAMBA_SOLANA_RPC_WS, commitment: 'processed' }}
+        config={{
+          wsEndpoint: import.meta.env.GAMBA_SOLANA_RPC_WS,
+          commitment: "processed",
+        }}
       >
         <WalletProvider autoConnect wallets={wallets}>
           <WalletModalProvider>
@@ -34,8 +38,8 @@ function Root() {
           </WalletModalProvider>
         </WalletProvider>
       </ConnectionProvider>
-    </BrowserRouter>
-  )
+    </MemoryRouter>
+  );
 }
 
-root.render(<Root />)
+root.render(<Root />);
